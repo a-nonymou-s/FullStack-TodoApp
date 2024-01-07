571ddc6b1430bfa769b764477be163042bdd6678
anony <yassinevirtual@gmail.com>
2024-01-07 12:28:30 +0100

    Setup Modification

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express'); 
-const cors = require('cors'); 
 const mongoose = require('mongoose'); 
 require('dotenv').config(); 
 const Todo = require('./models/Todo');
@@ -8,7 +7,6 @@ const Todo = require('./models/Todo');
 const app = express(); 
 
 app.use(express.json()); 
-app.use(cors()); 
 
 const port = 4001; 
 
